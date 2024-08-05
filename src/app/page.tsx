8777a024730dd44209d29d7d390f1eb5bb55627f
arce <Joo Augusto>
2024-08-05 14:23:41 -0300

    change padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import List from '@/components/list'
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col p-24">
+		<main className="flex min-h-screen flex-col px-2 py-4">
 			<List />
 		</main>
 	)
