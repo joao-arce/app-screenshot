f981a06c08feed4d36d6ee1b9cde14b3761d68a5
arce <Joo Augusto>
2024-08-09 17:29:38 -0300

    feet: add card details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Items from '@/components/items'
 import List from '@/components/list'
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col px-2 py-4">
-			<List />
+			{/* <List /> */}
+			<Items />
 		</main>
 	)
 }
---
+++ src/components/items.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+import { Card, CardContent } from './ui/card'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { products } from '../data/product'
+
+type ProductType = {
+	id: number
+	urlImage: string
+	name: string
+}
+
+const Items = () => {
+	return (
+		<div className="flex gap-2 overflow-auto [&::-webkit-scrollbar]:hidden">
+			{products.map((p) => (
+				<Card key={p.id} className="min-w-[167px] rounded-2xl">
+					<CardContent className="p-0 px-1 pt-1">
+						<div className="relative h-[159px] w-full">
+							<Image
+								fill
+								className="object-cover rounded-2xl"
+								alt={p.name}
+								src={p.urlImage}
+							/>
+						</div>
+						<div className="py-2 px-1">
+							<h3 className="font-semibold truncate my-2">{p.name}</h3>
+							<p className="text-sm">
+								<span className="font-semibold">color:</span> {p.color}
+							</p>
+							<p className="text-sm">
+								<span className="font-semibold">Tamanho:</span> {p.size}
+							</p>
+							<p className="text-sm">
+								<span className="font-semibold">Marca:</span> {p.trand}
+							</p>
+							<p className="text-sm">
+								<span className="font-semibold">Estoque:</span> {p.storage}
+							</p>
+						</div>
+					</CardContent>
+				</Card>
+			))}
+		</div>
+	)
+}
+
+export default Items
