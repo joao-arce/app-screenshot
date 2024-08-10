b3f4109e54f4c876da014b2060ffc74bd52bbd54
arce <Joo Augusto>
2024-08-10 15:01:36 -0300

    fix: correcting card size

--- src/components/items.tsx
+++ src/components/items.tsx
@@ -14,9 +14,9 @@ const Items = () => {
 	return (
 		<div className="flex gap-2 overflow-auto [&::-webkit-scrollbar]:hidden">
 			{products.map((p) => (
-				<Card key={p.id} className="min-w-[180px] rounded-2xl">
+				<Card key={p.id} className="min-w-[187px] rounded-2xl">
 					<CardContent className="p-0 px-1 pt-1">
-						<div className="h-[180px] w-full">
+						<div className="relative h-[185px] w-full">
 							<Image
 								fill
 								className="object-cover rounded-2xl"
